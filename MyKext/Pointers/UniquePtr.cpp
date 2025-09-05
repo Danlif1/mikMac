ac9a75e6386c967645dfc38995ed2f3c3061e5f5
Daniel Lifshitz <danlif1302@gmail.com>
2025-09-05 11:53:19 +0300

    dstd: UniquePtr: Add unique pointers to safely hold allocated memory

--- MyKext/Pointers/UniquePtr.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  UniquePtr.cpp
-//  MyKext
-//
-//  Created by Daniel Lifshitz on 22/08/2025.
-//
-
-#include "UniquePtr.hpp"
--- MyKext/Pointers/UniquePtr.hpp
+++ MyKext/Pointers/UniquePtr.hpp
@@ -4,10 +4,113 @@
 //
 //  Created by Daniel Lifshitz on 22/08/2025.
 //
+#pragma once
 
-#ifndef UniquePtr_hpp
-#define UniquePtr_hpp
+#include "../Checkers.hpp"
+#include "../Result.hpp"
 
-#include <stdio.h>
 
-#endif /* UniquePtr_hpp */
+namespace dstd {
+
+template<typename T, typename Deleter = DefaultDeleter<T>>
+class UniquePtr {
+public:
+    template<typename... Args>
+    static Result<UniquePtr<T>> make(Args... args) {
+        return makeArray(1, args...);
+    }
+    
+    template<typename... Args>
+    static Result<UniquePtr<T>> makeArray(const size_t size, Args... args) {
+        return makeArrayWithDeleter(DefaultDeleter<T>(), size, args...);
+    }
+    
+    template<typename... Args>
+    static Result<UniquePtr<T, Deleter>> makeArrayWithDeleter(Deleter deleter, const size_t size, Args... args) {
+        void* raw = operator new[](size * sizeof(T));
+        GENERIC_CHECK(nullptr != raw, KERN_NO_SPACE);
+        
+        auto array = static_cast<T*>(raw);
+        
+        for (size_t i = 0; i < size; i++) {
+            new (&array[i]) T(args...);
+        }
+        
+        UniquePtr result(array, size, deleter);
+        return Result<UniquePtr<T, Deleter>>::make(move(result));
+    }
+    
+    UniquePtr(UniquePtr&) = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+    
+    UniquePtr(UniquePtr&& other)
+        : m_value(other.m_value)
+        , m_size(other.m_size)
+        , m_deleter(other.m_deleter)
+    {
+        other.m_value = nullptr;
+        other.m_size = 0;
+    }
+    
+    UniquePtr& operator=(UniquePtr&& other) {
+        if (this == &other) {
+            return *this;
+        }
+        
+        reset();
+        
+        m_value = other.m_value;
+        m_size = other.m_size;
+        m_deleter = other.m_deleter;
+        
+        other.m_value = nullptr;
+        other.m_size = 0;
+    }
+    
+    ~UniquePtr() {
+        reset();
+    }
+    
+    T* operator*() {
+        return m_value;
+    }
+    
+    T* operator->() {
+        return m_value;
+    }
+    
+    T* getValue() {
+        return m_value;
+    }
+    
+    size_t getSize() {
+        return m_size;
+    }
+    
+private:
+    UniquePtr(T* value, size_t size, Deleter deleter)
+        : m_value(value)
+        , m_size(size)
+        , m_deleter(deleter)
+    {}
+    
+    void reset() {
+        if (nullptr == m_value) {
+            return;
+        }
+
+        for (size_t i = 0; i < m_size; ++i) {
+            m_deleter(&m_value[i]);
+        }
+        operator delete[](m_value);
+        
+        m_value = nullptr;
+        m_size = 0;
+    }
+    
+    T* m_value;
+    size_t m_size;
+    Deleter m_deleter;
+};
+
+} // namespace dstd
