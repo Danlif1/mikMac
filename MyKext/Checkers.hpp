7148f7bf27e2f9c3247d43a3664a6bda671bb173
Daniel Lifshitz <danlif1302@gmail.com>
2025-09-06 15:11:14 +0300

    dstd: Checkers: Add logs for the checks if failed

--- MyKext/Checkers.hpp
+++ MyKext/Checkers.hpp
@@ -8,6 +8,7 @@
 #define GENERIC_CHECK(action, errorValue) \
 { \
     if(!(action)) { \
+        LOG(5, "Action failed, error value: %d", errorValue); \
         return dstd::Result<void>::makeError((errorValue)); \
     } \
 }
@@ -15,6 +16,7 @@
 #define CHECK_RESULT(resultName, action) \
     auto resultName##__result = (action); \
     if (resultName##__result.hasError()) { \
+        LOG(5, "Action failed, error value: %d", resultName##__result.error()); \
         return dstd::Result<void>::makeError(resultName##__result.error());\
     } \
     auto resultName = dstd::move(resultName##__result.value())
