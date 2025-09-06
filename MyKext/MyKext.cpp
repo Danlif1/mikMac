68ea9ea7ed341c04d31f8cd1387204bfab0bb035
Daniel Lifshitz <danlif1302@gmail.com>
2025-09-06 15:14:47 +0300

    MyKext: Add usage of the KauthCallback example

--- MyKext/MyKext.cpp
+++ MyKext/MyKext.cpp
@@ -5,60 +5,46 @@
 //  Created by Daniel Lifshitz on 14/06/2025.
 //
 #include <sys/systm.h>
+#include <sys/vnode.h>
 #include <mach/mach_types.h>
  
+ 
+#include "Context.hpp"
+
 #include "Logger.h"
-#include "Optional.hpp"
+#include "Result.hpp"
+
+#include "Pointers/UniquePtr.hpp"
+#include "Pointers/SharedPtr.hpp"
+
+#include "KauthCallbacks/KauthCallback.hpp"
+#include "KauthCallbacks/Example.hpp"
+
+Context* g_context = nullptr;
 
 
-int* g_int;
-
-dstd::Optional<int> function(bool shouldReturn, int toReturn) {
-    if (shouldReturn) {
-        return dstd::Optional<int>(toReturn);
-    } else {
-        return {};
-    }
+dstd::Result<void> startImp(kmod_info_t * kmodInfo, void * data) {
+    CHECK_RESULT(executionCallback, registerExecutionPreventorExample());
+    g_context = new Context(dstd::move(executionCallback));
+    
+    return {};
 }
 
-extern "C" kern_return_t MyKext_start (kmod_info_t * ki, void * d)
+extern "C" kern_return_t MyKext_start (kmod_info_t * kmodInfo, void * data)
 {
-    function(true, 5);
-    auto first = function(true, 7);
-    auto second = function(false, 8);
-    
-    if (first.isValid()) {
-        auto fullFirst = first.get();
-        LOG(0, "first %d", *fullFirst);
-    }
-    
-    if (!second.isValid()) {
-        LOG(0, "second invalid");
-    }
-    
-    g_int = new int(5);
-    if (g_int) {
-        size_t x = reinterpret_cast<size_t>(g_int);
-        
-        while (x > 0) {
-            LOG(0, "%d", x % 10);
-            x /= 10;
-        }
-        LOG(0, "new works, p: %llu, d: %d", g_int, *g_int);
-    }
-    LOG(0, "starting with info ki: %p, d: %p", ki, d);
-    return KERN_SUCCESS;
+    LOG(0, "starting with info kmodInfo: %p, data: %p", kmodInfo, data);
+    return startImp(kmodInfo, data);
 }
  
-extern "C" kern_return_t MyKext_stop (kmod_info_t * ki, void * d)
+extern "C" kern_return_t MyKext_stop (kmod_info_t * kmodInfo, void * data)
 {
-    if (nullptr != g_int) {
-        LOG(0, "g_int: %llu, value %d", g_int, *g_int);
-        delete g_int;
+    if (nullptr != g_context) {
+        delete g_context;
+        g_context = nullptr;
     }
     
     LOG(1, "stopping");
-    LOG(2, "Also stopping with value: ki = %p", ki);
+    LOG(2, "Also stopping with value: kmodInfo = %p", kmodInfo);
     printf("MyKext has stopped.\n");
     return KERN_SUCCESS;
 }
